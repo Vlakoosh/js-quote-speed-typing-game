8507814b1ebe18394c190019c064650f7e2b1e40
vlakoosh <vlakoosh@gmail.com>
2024-02-06 09:30:06 +0100

    back to http url

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
+const RANDOM_QUOTE_API_URL = 'http://api.quotable.io/random';
 const quoteDisplayElement = document.querySelector('.quote-display');
 const quoteInputElement = document.querySelector('.quote-input');
 const timerElement = document.querySelector('.timer');
